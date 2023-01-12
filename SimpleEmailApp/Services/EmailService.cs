686547e614b79ed879de49e4e59f2875614ba239
omarsilvayepes <omar.yepes@hotmail.com>
2023-01-12 18:06:42 -0500

    Add send Attachments

--- SimpleEmailApp/Services/EmailService.cs
+++ SimpleEmailApp/Services/EmailService.cs
@@ -1,7 +1,7 @@
 ﻿using MailKit.Security;
-using MimeKit.Text;
 using MimeKit;
 using MailKit.Net.Smtp;
+using MimeKit.Text;
 
 namespace SimpleEmailApp.Services
 {
@@ -16,11 +16,39 @@ namespace SimpleEmailApp.Services
 
         public string SendEmail(string body)
         {
-                var email = new MimeMessage();
+
+            using(var email=new MimeMessage())
+            {
+                // configure the sender, recipient, subject,attachment ,and body of the message
+
                 email.From.Add(MailboxAddress.Parse(configuration.GetSection("SenderEmail").Value));
                 email.To.Add(MailboxAddress.Parse(configuration.GetSection("ReceiverEmail").Value));
                 email.Subject = configuration.GetSection("Subject").Value;
-                email.Body = new TextPart(TextFormat.Text) { Text = body };
+
+                var multipart = new Multipart("mixed");
+
+                // add the text body
+                multipart.Add(new TextPart(TextFormat.Text)//  set "plain or any format 
+                {
+                    Text = body
+                });
+
+                //Build attachment  Path 
+
+                string FileName = configuration.GetSection("FileName").Value;
+                string path = Path.Combine("Files/", FileName);
+
+                // add the attachments
+                var attachment = new MimePart()
+                {
+                    Content = new MimeContent(File.OpenRead(path)),
+                    ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                    ContentTransferEncoding = ContentEncoding.Base64,
+                    FileName = FileName
+                };
+                multipart.Add(attachment);
+
+                email.Body = multipart;
 
                 using var smtp = new SmtpClient();
                 smtp.Connect(configuration.GetSection("Host").Value, Int32.Parse(configuration.GetSection("Port").Value), SecureSocketOptions.Auto);
@@ -28,6 +56,7 @@ namespace SimpleEmailApp.Services
                 smtp.Send(email);
                 smtp.Disconnect(true);
                 return "Success";
+            }
         }
     }
 }
